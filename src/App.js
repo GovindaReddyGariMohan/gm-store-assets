9ca099a7103aa8401baa085c35d365d8f5c5862e
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2025-05-07 16:30:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import head1 from './assets/head-1.jpeg'
+function App() {
+  return (
+    <div className="App">
+     <img src={head1} alt=''/>
+    </div>
+  );
+}
+
+export default App;
