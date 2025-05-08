1d51fc713a56bf60ddce8296be89edd72ddc513c
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2025-05-08 17:13:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import head1 from './assets/head-1.jpeg'
+import image11 from './assets/section-1-1.jpeg'
 function App() {
   return (
     <div className="App">
      <img src={head1} alt=''/>
+     <img src={image11} alt=''/>
     </div>
   );
 }
